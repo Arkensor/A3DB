f7084bad74e1cb88b02ba5ac8f141af62e7da5ff
BoGuu <boguug@gmail.com>
2017-02-04 13:25:57 +0000

    Spaces -> Tabs Processor.hpp

--- src/A3DB/Processor.hpp
+++ src/A3DB/Processor.hpp
@@ -14,72 +14,72 @@ public:
 
 	Processor(Extension *p) : ext(p) {}
 
-    void start(std::function<std::vector<U>(T)> f, int c)
-    {
-        if (started) return;
+	void start(std::function<std::vector<U>(T)> f, int c)
+	{
+		if (started) return;
 
-        int expected = std::thread::hardware_concurrency();
+		int expected = std::thread::hardware_concurrency();
 
-        if (c == -1 || c > (int)expected) 
-            c = expected;
+		if (c == -1 || c > (int)expected)
+			c = expected;
 
-		#ifdef EXTENSION_DEBUG_OUTPUT_WINDOWS
+#ifdef EXTENSION_DEBUG_OUTPUT_WINDOWS
 		ext->console->info("Loading worker threads..");
-		#endif
+#endif
 
-        for (int i = 0; i < c; i++)
-        {
-            std::thread t(&Processor::run, this, f);
-            t.detach();
-        }
+		for (int i = 0; i < c; i++)
+		{
+			std::thread t(&Processor::run, this, f);
+			t.detach();
+		}
 
-        started = true;
+		started = true;
 
-		#ifdef EXTENSION_DEBUG_OUTPUT_WINDOWS
+#ifdef EXTENSION_DEBUG_OUTPUT_WINDOWS
 		ext->console->info("Worker threads loaded!");
-		#endif
-    }
+#endif
+	}
 
-    void add(T r)
-    {
-        request_q.enqueue(r);
-    }
+	void add(T r)
+	{
+		request_q.enqueue(r);
+	}
 
-    bool try_get_result(U& r)
-    {
-        return result_q.try_pop_result(r);
-    }
+	bool try_get_result(U& r)
+	{
+		return result_q.try_pop_result(r);
+	}
 
-    size_t request_size()
-    {
-        return result_q.size();
-    }
+	size_t request_size()
+	{
+		return result_q.size();
+	}
 
-    size_t result_size()
-    {
-        return result_q.size();
-    }
+	size_t result_size()
+	{
+		return result_q.size();
+	}
 
 private:
 
-    void run(std::function<std::vector<U>(T)> f)
-    {
-        while (true)
-        {
-            T arg = request_q.dequeue();
-            std::vector<U> results = f(arg);
-            for (U result : results)
-            {
-                result_q.enqueue(result);
-            }
-        }
-    }
+	void run(std::function<std::vector<U>(T)> f)
+	{
+		while (true)
+		{
+			T arg = request_q.dequeue();
+			std::vector<U> results = f(arg);
+			for (U result : results)
+			{
+				result_q.enqueue(result);
+			}
+		}
+	}
 
-    bool started = false;
+	bool started = false;
 
 	Extension *ext;
 
-    SafeQueue<U> result_q;
-    SafeQueue<T> request_q;
+	SafeQueue<U> result_q;
+	SafeQueue<T> request_q;
 };
 #endif
