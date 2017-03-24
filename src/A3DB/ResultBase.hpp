821ee6dd27145c791fc0230d22486c481f969e27
Paul-Eric Lange <arkensor@gmail.com>
2017-03-24 15:40:37 +0100

    one header to rule them all

--- src/A3DB/ResultBase.hpp
+++ src/A3DB/ResultBase.hpp
@@ -1,3 +1,13 @@
+/*
+		_    _____  ____  ____
+	   / \  |___ / |  _ \| __ )
+	  / _ \   |_ \ | | | |  _ \
+	 / ___ \ ___)  | |_| | |_) |
+	/_/   \_\____/ |____/|____/
+
+	a cross database extension for Arma 3 by Arkensor
+*/
+
 #ifndef __RESULT_BASE_H
 #define __RESULT_BASE_H
 
